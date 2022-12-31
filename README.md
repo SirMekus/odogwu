e87f80ce4d5fd4b1a65a31c0e86c53e20d49458f
webloit-dev <webloit2020@gmail.com>
2022-12-31 16:34:16 +0100

    Updated the ReadMe file for direction on how to use this package

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Installation
 
+This is a test project to get you started in exploring the power of two of my open-source packages - [Ahia](https://github.com/SirMekus/ahia) and [Zam](https://github.com/SirMekus/zam). Also, you can build a fresh 'native' PHP application off this project; just download and build on it (bundling and directory structure has already been created but you can edit it to your taste. Refer to the packages' documentation for more infomation).
+
 1. Run `composer install`
 1. Run `npm install`
 1. Create your `.env` file from the `.env.example` file downloaded
+1. Run the dump.sql file (or import it to your database)
 1. Enjoy your surfing
---
+++ dump.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 30, 2022 at 12:29 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 8.1.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `reg`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+
+CREATE TABLE `register` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dob` date NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cc` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `register`
+--
+ALTER TABLE `register`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `register`
+--
+ALTER TABLE `register`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
